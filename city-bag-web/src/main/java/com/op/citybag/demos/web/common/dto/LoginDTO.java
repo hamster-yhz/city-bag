62a49a086aef523246270d8cebb7f956ef3f3023
hamster-yhz <1427739483@qq.com>
2025-01-20 19:43:39 +0800

    feat: DTO

---
+++ city-bag-web/src/main/java/com/op/citybag/demos/web/common/dto/LoginDTO.java
@@ -0,0 +1,35 @@
+package com.op.citybag.demos.web.common.dto;
+
+import lombok.*;
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+/**
+ * @Author: 原神
+ * @Description:
+ * @Date: 2025/1/18 20:06
+ * @Version: 1.0
+ */
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Validated
+public class LoginDTO {
+
+    /**
+     * 微信登陆零时码code，通过wx.login() 获取
+     */
+    @NotBlank(message = "code不能为空")
+    String code;
+
+    /**
+     * 微信登陆获取手机号的code, 通过wx.getPhoneNumber() 获取
+     */
+    @NotBlank(message = "phoneCode不能为空")
+    String phoneCode;
+}
