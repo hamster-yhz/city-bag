c259cd8615cbcf09e0761815411c51ae89fd491c
hamster-yhz <1427739483@qq.com>
2025-02-28 19:50:43 +0800

    feat: mp自动填充类配置

---
+++ city-bag-start/src/main/java/com/op/citybag/config/AutoFillHandler.java
@@ -0,0 +1,29 @@
+package com.op.citybag.config;
+
+/**
+ * @Author: 原神
+ * @Description:
+ * @Date: 2025/2/27 18:56
+ * @Version: 1.0
+ */
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Component
+public class AutoFillHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+    }
+}
